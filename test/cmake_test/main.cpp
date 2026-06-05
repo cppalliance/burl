34c6883d709b92500f2a7d3a5a56e6149d678012
Mohammad Nejati <ashtumashtum@gmail.com>
2026-06-05 12:55:49 +0000

    add a dummy test

--- test/cmake_test/main.cpp
+++ test/cmake_test/main.cpp
@@ -1,7 +1,9 @@
 #include <boost/burl.hpp>
 
-int main() {
-  std::error_code ec = boost::burl::error::success;
-  if (ec)
-    throw;
+int
+main()
+{
+    std::error_code ec = boost::burl::error::bad_redirect_response;
+    if(!ec)
+        throw;
 }
